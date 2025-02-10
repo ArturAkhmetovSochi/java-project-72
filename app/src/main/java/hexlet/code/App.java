1926367a45eaca68f4be0325e62aded81f920baf
Artur Akhmetov <artur.akh@internet.ru>
2025-02-10 12:37:21 +0300

    create getApp, getPort and main methods in App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -22,9 +22,24 @@ import static io.javalin.rendering.template.TemplateUtil.model;
 @Slf4j
 public class App {
 
-    public static void main(String[] args) throws IOException, SQLException {
-        var app = getApp();
+    private static int getPort() {
+        String port = System.getenv().getOrDefault("PORT", "7070");
+        return Integer.valueOf(port);
     }
 
+    public static void main(String[] args) throws IOException, SQLException {
+        var app = getApp();
 
+        app.start(getPort());
+    }
+
+    public static Javalin getApp() throws  IOException, SQLException {
+        var app = Javalin.create(config -> {
+            config.bundledPlugins.enableDevLogging();
+            config.fileRenderer(new JavalinJte());
+        });
+        app.get("/", ctx -> ctx.result("Katya my love!"));
+
+        return app;
+    }
 }
